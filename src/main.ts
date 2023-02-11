cb6ad5d23e28bf3abefbf3b3ef1ddef4eeb6230a
Will Martin <williammartinthird@gmail.com>
2023-02-11 14:59:12 -0700

    Display user settings on the settings page
    Also added text prompts.

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,12 @@ getSWRegistration()
           { h: 14, m: 0 },
           { h: 19, m: 0 },
           { h: 23, m: 0 },
-        ]
+        ],
+        dailyPrompts: [
+          {type: 'text', prompt: 'What is one thing you wish you did better today?'},
+          {type: 'text', prompt: 'What is one thing you are proud of today?'},
+          {type: 'text', prompt: 'What are you looking forward to tomorrow?'},
+        ],
       })
     }
   });
--- src/storage/userSettings.ts
+++ src/storage/userSettings.ts
@@ -1,6 +1,9 @@
+import { writable } from "svelte/store";
 import { sendMessageToServiceWorker } from "../serviceWorker/clientSide";
 
 const userSettingsKey = 'user-settings';
+
+export const userSettingsWatcher = writable(getUserSettings());
 
 /**
  * The time for a check-in notification in the local timezone.
@@ -12,8 +15,16 @@ interface CheckInTime {
   m: number;
 }
 
+interface TextPrompt {
+  type: 'text',
+  prompt: string,
+}
+
+type Prompt = TextPrompt;
+
 export interface UserSettings {
   checkInTimes: CheckInTime[];
+  dailyPrompts: Prompt[];
 }
 
 export function getUserSettings(): UserSettings | null {
@@ -26,6 +37,7 @@ export function getUserSettings(): UserSettings | null {
 
 export async function setUserSettings(settings: UserSettings): Promise<void> {
   localStorage.setItem(userSettingsKey, JSON.stringify(settings));
+  userSettingsWatcher.set(settings);
   sendMessageToServiceWorker({
     subject: 'user-settings',
     settings: settings,
