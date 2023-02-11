e7f2dc956a196ffedc554b810b3225fdb7f1b771
William Martin <williammartinthird@gmail.com>
2023-02-11 13:42:25 -0700

    Add option to force update app (#29)
    Chrome Android has an issue where it won't update for some reason.
    Also simplified client side SW code.

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -2,34 +2,46 @@
   import Button from "../components/Button.svelte";
   import Page from "../components/Page.svelte";
   import { transitionTo } from "../navigation";
-  import { sendMessageToServiceWorker } from "../serviceWorker/messagePassing";
+  import {
+    getSWRegistration,
+    sendMessageToServiceWorker,
+  } from "../serviceWorker/clientSide";
 
   $: permission = Notification.permission;
+
+  const forceUpdate = async () => {
+    const registration = await getSWRegistration();
+    await registration.update();
+    location.reload();
+  };
 </script>
 
 <Page>
   <h1 slot="header">Settings</h1>
 
-  <div class="buttonRow">
-    {#if permission === "granted"}
-      <Button disabled flex="1">Notifications Enabled!</Button>
-    {:else}
+  <div class="contents">
+    <div class="buttonRow">
+      {#if permission === "granted"}
+        <Button disabled flex="1">Notifications Enabled!</Button>
+      {:else}
+        <Button
+          flex="1"
+          onClick={() =>
+            Notification.requestPermission().then((p) => (permission = p))}
+        >
+          Enable Notifications
+        </Button>
+      {/if}
       <Button
+        disabled={permission !== "granted"}
         flex="1"
         onClick={() =>
-          Notification.requestPermission().then((p) => (permission = p))}
+          sendMessageToServiceWorker({ subject: "test-notification" })}
       >
-        Enable Notifications
+        Test notifications
       </Button>
-    {/if}
-    <Button
-      disabled={permission !== "granted"}
-      flex="1"
-      onClick={() =>
-        sendMessageToServiceWorker({ subject: "test-notification" })}
-    >
-      Test notifications
-    </Button>
+    </div>
+    <Button onClick={forceUpdate}>Force app update</Button>
   </div>
 
   <div slot="footer" class="footer buttonRow">
@@ -43,6 +55,11 @@
 </Page>
 
 <style>
+  .contents {
+    display: flex;
+    flex-direction: column;
+    gap: var(--padding-small);
+  }
   .buttonRow {
     display: flex;
     flex-direction: row;
