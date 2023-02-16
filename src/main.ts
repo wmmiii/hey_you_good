f30012e2754a047bc41cfc4689c71b4d306e2bcf
William Martin <williammartinthird@gmail.com>
2023-02-16 00:23:30 -0700

    Always assign default settings
    This will fix when clients don't have a default option for one of their
    user settings fields.
    Also fixed buttons on Settings page.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./browserHistory";
 import App from "./App.svelte";
 import { initStorage } from './storage/localDb';
-import { getUserSettings, setUserSettings } from "./storage/userSettings";
+import { getUserSettings, setUserSettings, UserSettings } from "./storage/userSettings";
 import { getSWRegistration, initMessageListener } from "./serviceWorker/clientSide";
 
 initStorage();
@@ -10,21 +10,21 @@ initStorage();
 getSWRegistration()
   .then(() => {
     initMessageListener();
-    if (getUserSettings() == null) {
-      setUserSettings({
-        checkInTimes: [
-          { h: 10, m: 0 },
-          { h: 14, m: 0 },
-          { h: 19, m: 0 },
-          { h: 23, m: 0 },
-        ],
-        dailyPrompts: [
-          {type: 'text', prompt: 'What is one thing you wish you did better today?'},
-          {type: 'text', prompt: 'What is one thing you are proud of today?'},
-          {type: 'text', prompt: 'What are you looking forward to tomorrow?'},
-        ],
-      })
-    }
+    const defaultSettings: UserSettings = {
+      checkInTimes: [
+        { h: 10, m: 0 },
+        { h: 14, m: 0 },
+        { h: 19, m: 0 },
+        { h: 23, m: 0 },
+      ],
+      dailyPrompts: [
+        { type: 'text', prompt: 'What is one thing you wish you did better today?' },
+        { type: 'text', prompt: 'What is one thing you are proud of today?' },
+        { type: 'text', prompt: 'What are you looking forward to tomorrow?' },
+      ],
+    };
+
+    setUserSettings(Object.assign({}, defaultSettings, getUserSettings()));
   });
 
 new App({
