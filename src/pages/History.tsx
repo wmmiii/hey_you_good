81dccc82b59566e45e7bd2dd5ad96a6cfaa439c4
Will Martin <williammartinthird@gmail.com>
2023-12-07 06:31:54 -0700

    Update history page to React

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,7 +1,94 @@
-import React from 'react';
+import Button from '../components/Button';
+import Page from '../components/Page';
+import React, { useEffect, useMemo, useState } from 'react';
+import styles from './History.module.scss';
+import { Feeling, getFeelings } from '../storage/localDb';
+import { gloriaIndex } from "../feelingsModel";
+import { useNavigate } from 'react-router';
 
 export default function History(): JSX.Element {
+  const navigate = useNavigate();
+  const [feelings, setFeelings] = useState<Feeling[] | null>(null);
+
+  useEffect(() => {
+    const now = new Date();
+    const yearAgo = new Date(now);
+    yearAgo.setFullYear(yearAgo.getFullYear() - 1);
+
+    getFeelings(now, yearAgo).then(setFeelings);
+  });
+
+  const days = useMemo((): Day[] => {
+    if (feelings == null) {
+      return [];
+    }
+
+    return feelings
+      .sort((a, b) => b.ts.getTime() - a.ts.getTime())
+      .reduce((list, entry) => {
+        const entryDay = new Date(entry.ts.getTime());
+        entryDay.setHours(0);
+        entryDay.setMinutes(0);
+        entryDay.setSeconds(0);
+        entryDay.setMilliseconds(0);
+
+        if (list.length > 0) {
+          const lastEntry = list[list.length - 1];
+          if (lastEntry.date.getTime() === entryDay.getTime()) {
+            lastEntry.entries.push(entry);
+            return list;
+          }
+        }
+        list.push({
+          date: entryDay,
+          entries: [entry],
+        });
+        return list;
+      }, [] as Day[])
+  }, [feelings]);
+
+
   return (
-    <h1>History</h1>
+    <Page
+      footer={
+        <Button
+          flex="0 auto 0"
+          onClick={() => navigate('/')}>
+          Back
+        </Button>
+      }>
+      {
+        days == null ?
+          <span>Loading...</span> :
+          <ol>
+            {
+              days.map((d, i) => (
+                <li key={i}>
+                  <h3>{d.date.toLocaleDateString()}</h3>
+                  <ul>
+                    {
+                      d.entries.map((e, i) => (
+                        <div
+                          key={i}
+                          className={styles.feeling}
+                          style={{
+                            backgroundColor: gloriaIndex[e['path'][0]].color
+                          }}>
+                          {e["path"][e["path"].length - 1]}
+                        </div>
+                      ))
+                    }
+                  </ul>
+                </li>
+              ))
+            }
+          </ol>
+      }
+    </Page>
   );
+}
+
+interface Day {
+  date: Date;
+  entries: Feeling[];
 }
